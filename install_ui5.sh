3ab1af0ed3b576cd2d0e1b5a7dc6e2ffae107a8d
D067894 <christoph.neumaier@sap.com>
2018-02-01 16:34:30 +0100

    updated UI5 version

--- install_ui5.sh
+++ install_ui5.sh
@@ -3,6 +3,6 @@
 
 cd /var/www/html
 rm index.html
-wget https://openui5.hana.ondemand.com/downloads/openui5-runtime-1.50.8.zip
+wget https://openui5.hana.ondemand.com/downloads/openui5-runtime-1.52.5.zip
 unzip openui5-runtime-1.50.8.zip
 rm openui5-runtime-1.50.8.zip
