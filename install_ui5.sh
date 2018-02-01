bc19761269fa1fd0448593902c0ebad959190d6e
D067894 <christoph.neumaier@sap.com>
2018-02-01 16:48:52 +0100

    fixed ui5 version bug

--- install_ui5.sh
+++ install_ui5.sh
@@ -4,5 +4,5 @@
 cd /var/www/html
 rm index.html
 wget https://openui5.hana.ondemand.com/downloads/openui5-runtime-1.52.5.zip
-unzip openui5-runtime-1.50.8.zip
-rm openui5-runtime-1.50.8.zip
+unzip openui5-runtime-1.52.5.zip
+rm openui5-runtime-1.52.5.zip
